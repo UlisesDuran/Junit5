583571331ff843dd9cdbc1fb48e8d194b6a8a743
Ulises Duran <72855605+AlexSpecter@users.noreply.github.com>
2023-11-26 22:17:45 +0100

    Agregando la prueba unitaria para la clase cuenta y validamos la persona y el saldo

--- src/test/java/com/uduran/junit5app/ejemplos/CuentaTest.java
+++ src/test/java/com/uduran/junit5app/ejemplos/CuentaTest.java
@@ -1,10 +1,28 @@
 package com.uduran.junit5app.ejemplos;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CuentaTest {
+
+    @Test
+    void testCuenta() {
+        Cuenta cuenta = new Cuenta();
+        cuenta.setPersona("Ulises");
+        String esperado = "Ulises";
+        String real = cuenta.getPersona();
+        Assertions.assertEquals(esperado, real);
+    }
+
+    void testSaldoCuenta(){
+        Cuenta cuenta = new Cuenta("Ulises", new BigDecimal("1000.12345"));
+        assertEquals(1000.12345, cuenta.getSaldo().doubleValue());
+        assertFalse(cuenta.getSaldo().compareTo(BigDecimal.ZERO) < 0);
+    }
 
     @Test
     void getPersona() {
