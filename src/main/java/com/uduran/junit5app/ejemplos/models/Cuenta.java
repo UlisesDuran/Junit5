3ded041e64b446b42a1f8d99fa8c785b1fc8bdc0
Ulises Duran <72855605+AlexSpecter@users.noreply.github.com>
2023-11-29 09:17:51 +0100

    Agregando trabajo en remoto con assertAll + programacion funcional

---
+++ src/main/java/com/uduran/junit5app/ejemplos/models/Cuenta.java
@@ -0,0 +1,78 @@
+package com.uduran.junit5app.ejemplos.models;
+
+import com.uduran.junit5app.ejemplos.exceptions.DineroInsuficienteException;
+
+import java.math.BigDecimal;
+import java.util.Objects;
+
+public class Cuenta {
+    private String persona;
+    //Trabajamos con este por que de esta forma es mucho mas preciso para trabajar con dinero.
+    private BigDecimal saldo;
+    private Banco banco;
+
+    public Cuenta() {
+    }
+
+    public Cuenta(String persona, BigDecimal saldo) {
+        this.persona = persona;
+        this.saldo = saldo;
+    }
+
+    public String getPersona() {
+        return persona;
+    }
+
+    public void setPersona(String persona) {
+        this.persona = persona;
+    }
+
+    public BigDecimal getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(BigDecimal saldo) {
+        this.saldo = saldo;
+    }
+
+    public Banco getBanco() {
+        return banco;
+    }
+
+    public void setBanco(Banco banco) {
+        this.banco = banco;
+    }
+
+    @Override
+    public String toString() {
+        return persona + ", saldo=" + saldo;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (object == null || getClass() != object.getClass()) return false;
+        Cuenta cuenta = (Cuenta) object;
+        return Objects.equals(persona, cuenta.persona) && Objects.equals(saldo, cuenta.saldo);
+    }
+
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(persona, saldo);
+    }
+
+    public void debito(BigDecimal monto){
+        BigDecimal nuevosaldo = this.saldo.subtract(monto);
+        if(nuevosaldo.compareTo(BigDecimal.ZERO) < 0){
+            throw new DineroInsuficienteException("Dinero Insuficiente");
+        }
+        this.saldo = nuevosaldo;
+    }
+
+    public void credito(BigDecimal monto){
+        this.saldo = this.saldo.add(monto);
+    }
+
+}
+
--- src/test/java/com/uduran/junit5app/ejemplos/CuentaTest.java
+++ src/test/java/com/uduran/junit5app/ejemplos/CuentaTest.java
@@ -1,5 +1,8 @@
 package com.uduran.junit5app.ejemplos;
 
+import com.uduran.junit5app.ejemplos.exceptions.DineroInsuficienteException;
+import com.uduran.junit5app.ejemplos.models.Banco;
+import com.uduran.junit5app.ejemplos.models.Cuenta;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -17,17 +20,87 @@ class CuentaTest {
         String real = cuenta.getPersona();
         Assertions.assertEquals(esperado, real);
     }
+
     @Test
-    void testSaldoCuenta(){
+    void testSaldoCuenta() {
         Cuenta cuenta = new Cuenta("Ulises", new BigDecimal("1000.12345"));
-        assertEquals(1000.12345, cuenta.getSaldo().doubleValue());
-        assertFalse(cuenta.getSaldo().compareTo(BigDecimal.ZERO) < 0);
+        assertAll(
+                () -> assertEquals(1000.12345, cuenta.getSaldo().doubleValue()),
+                () -> assertFalse(cuenta.getSaldo().compareTo(BigDecimal.ZERO) < 0));
     }
+
     @Test
-    void testReferenciaCuenta(){
-        Cuenta cuenta = new Cuenta("Jong Doe", new BigDecimal("8900.9997"));
-        Cuenta cuenta1 = new Cuenta("Jong Doe", new BigDecimal("8900.9997"));
+    void testReferenciaCuenta() {
+        Cuenta cuenta = new Cuenta("Jon Doe", new BigDecimal("8900.9997"));
+        Cuenta cuenta1 = new Cuenta("Jon Doe", new BigDecimal("8900.9997"));
         assertEquals(cuenta1, cuenta);
+    }
+
+    @Test
+    void testDebitocuenta() {
+        Cuenta cuenta = new Cuenta("Andres", new BigDecimal("1000.12345"));
+        cuenta.debito(new BigDecimal(100));
+        assertAll(
+                () -> assertNotNull(cuenta.getSaldo()),
+                () -> assertEquals(900, cuenta.getSaldo().intValue()),
+                () -> assertEquals("900.12345", cuenta.getSaldo().toPlainString()));
+    }
+
+    @Test
+    void testCreditoCuenta() {
+        Cuenta cuenta = new Cuenta("Andres", new BigDecimal("1000.12345"));
+        cuenta.credito(new BigDecimal(100));
+        assertAll(
+                () -> assertNotNull(cuenta.getSaldo()),
+                () -> assertEquals(1100, cuenta.getSaldo().intValue()),
+                () -> assertEquals("1100.12345", cuenta.getSaldo().toPlainString()));
+    }
+
+    @Test
+    void testDineroInsuficienteExceptionCuenta() {
+        Cuenta cuenta = new Cuenta("Ulises", new BigDecimal("1000.12345"));
+        Exception exception = assertThrows(DineroInsuficienteException.class, () -> {
+            cuenta.debito(new BigDecimal("1500"));
+        });
+        String actual = exception.getMessage();
+        String esperado = "Dinero Insuficiente";
+        assertEquals(actual, esperado, "La cuenta no tiene suficiente dinero!");
+    }
+
+    @Test
+    void testTrasnferirDineroCuenta() {
+        Cuenta cuenta1 = new Cuenta("John Doe", new BigDecimal("2500"));
+        Cuenta cuenta2 = new Cuenta("Ulises", new BigDecimal("1500"));
+
+        Banco banco = new Banco("BBVA");
+        banco.transferir(cuenta2, cuenta1, new BigDecimal(1000));
+        assertAll(
+                () -> assertEquals("500", cuenta2.getSaldo().toPlainString()),
+                () -> assertEquals("3500", cuenta1.getSaldo().toPlainString()));
+
+    }
+
+    @Test
+    void testRelacionBancoCuenta() {
+        Cuenta cuenta1 = new Cuenta("John Doe", new BigDecimal("2500"));
+        Cuenta cuenta2 = new Cuenta("Ulises", new BigDecimal("1500"));
+
+        Banco banco = new Banco("BBVA");
+        banco.addCuenta(cuenta1);
+        banco.addCuenta(cuenta2);
+        assertAll(
+                () -> assertEquals(2, banco.getCuentas().size()),
+                () -> assertEquals("BBVA", cuenta1.getBanco().getNombre()),
+                () -> {
+                    assertEquals("Ulises", banco.getCuentas().stream()
+                            .filter(c -> c.getPersona().equals("Ulises"))
+                            .findFirst().get().getPersona());
+                },
+                () -> {
+                    assertTrue(banco.getCuentas().stream()
+                            .anyMatch(c -> c.getPersona().equals("Ulises")));
+                });
+
     }
 
     @Test
